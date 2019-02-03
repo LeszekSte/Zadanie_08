3a76afe09dbe5405d64e43fbcd19a02eaa7461bd
Leszek <leszekstepien@o2.pl>
2019-02-03 12:13:09 +0100

    Wersja rozszerzona

--- src/Zadanie4_dodatkowe/ArrayCompator.java
+++ src/Zadanie4_dodatkowe/ArrayCompator.java
@@ -2,20 +2,43 @@ package Zadanie4_dodatkowe;
 
 public class ArrayCompator {
 
-    static boolean compere(int [] tab1, int [] tab2){
+    static boolean compere(int[] tab1, int[] tab2) {
 
         boolean compere = true;
-        if (tab1.length != tab2.length){
+        if (tab1.length != tab2.length) {
             return compere = false;
-        }else {
-            int i=0;
+        } else {
+            int i = 0;
             do {
-                if (tab1[i]!=tab2[i]){
-                    return compere= false;
+                if (tab1[i] != tab2[i]) {
+                    return compere = false;
                 }
                 i++;
-            }while (i<tab1.length);
+            } while (i < tab1.length);
         }
-    return compere;
+        return compere;
+    }
+
+    static boolean compere(int[][] tab1, int[][] tab2) {
+        boolean compere = true;
+
+        if (tab1[0].length != tab2[0].length || tab1[1].length != tab2[1].length) {
+            return compere = false;
+        } else {
+            for (int i = 0; i < 2; i++) {
+                int j = 0;
+                do {
+                    if (tab1[i][j] != tab2[i][j]) {
+                        return compere = false;
+                    }
+                    j++;
+                } while (j < tab1[i].length);
+            }
+        }
+        return compere;
     }
 }
+
+
+
+
--- src/Zadanie4_dodatkowe/ArrayTest.java
+++ src/Zadanie4_dodatkowe/ArrayTest.java
@@ -9,7 +9,10 @@ public class ArrayTest {
         int[] tab1 = {1, 2, 3, 4};
         int[] tab2 = {1, 2, 3, 4};
 
-        System.out.println(ArrayCompator.compere(tab1,tab2));
+        int[] []tab3 = {{1, 2, 3, 4},{1,2,4}};
+        int[] []tab4 = {{1, 2, 3, 4},{1,2}};
 
+        System.out.println(ArrayCompator.compere(tab1,tab2));
+        System.out.println(ArrayCompator.compere(tab3,tab4));
     }
 }
